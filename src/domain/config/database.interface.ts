efc8765f3cedbaa4e1d1263c0802fe831f18a5b6
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-11 13:49:22 -0300

    add: adding database interface

---
+++ src/domain/config/database.interface.ts
@@ -0,0 +1,9 @@
+export interface DatabaseConfig {
+  getDataBaseHost(): string;
+  getDatabasePort(): number;
+  getDatabaseUser(): string;
+  getDatabasePassword(): string;
+  getDatabaseName(): string;
+  getDatabaseSchema(): string;
+  getDatabaseSync(): boolean;
+}
