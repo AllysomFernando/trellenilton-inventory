be57fb2441fb972f378b7aced415a5c963b7336f
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-11 15:17:03 -0300

    add: implementar repositório UserRepositoryOrm para acesso a dados de usuários

---
+++ src/infrastructures/repositories/user.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/domain/repository/user.repository';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from 'src/domain/models/user';
+
+@Injectable()
+export class UserRepositoryOrm implements UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async getAllUser(): Promise<User[]> {
+    const users = await this.userRepository.find();
+    return users.map((user) => this.toUser(user));
+  }
+
+  private toUser(userEntity: User): UserModel {
+    const user: UserModel = new UserModel();
+
+    user.id = userEntity.id;
+    user.email = userEntity.email;
+    user.name = userEntity.name;
+    user.password = userEntity.password;
+
+    return user;
+  }
+}
