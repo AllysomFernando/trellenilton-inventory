accf7df0e6ea99afbe7d94c9bd95d75b716b7dda
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-11 14:13:45 -0300

    refactor: update DatabaseConfig interface to include new properties

--- src/domain/config/database.interface.ts
+++ src/domain/config/database.interface.ts
@@ -1,9 +1,4 @@
 export interface DatabaseConfig {
-  getDataBaseHost(): string;
-  getDatabasePort(): number;
-  getDatabaseUser(): string;
-  getDatabasePassword(): string;
-  getDatabaseName(): string;
-  getDatabaseSchema(): string;
-  getDatabaseSync(): boolean;
+  getDatabaseType(): string;
+  getDatabasePath(): string;
 }
