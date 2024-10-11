373cb491b2741bf848870bd609beb90e92657810
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-11 15:18:52 -0300

    add: implementar caso de uso para obter todos os usuários

---
+++ src/applications/use-cases/user.usecase.ts
@@ -0,0 +1,10 @@
+import { UserModel } from "src/domain/models/user";
+import { UserRepository } from "src/domain/repository/user.repository";
+
+export class GetAllUsersUseCase {
+    constructor(private usersRepository: UserRepository){}
+
+    async execute(): Promise<UserModel[]>{
+        return await this.usersRepository.getAllUser();
+    }
+}
