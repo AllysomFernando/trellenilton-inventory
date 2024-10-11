e96b0dcb02f4d91120b8472d4bc340a2c50bb9b8
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-11 14:13:51 -0300

    add: create TypeOrmConfigModule for database configuration with SQLite support

---
+++ src/infra/config/typeorm/typeorm.module.ts
@@ -0,0 +1,27 @@
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { EnvironmentConfigService } from '../environment-config/environment-config.service';
+import { Module } from '@nestjs/common';
+import { EnvironmentConfigModule } from '../environment-config/environment-config.module';
+
+export const getTypeOrmModuleOptions = (
+  config: EnvironmentConfigService,
+): TypeOrmModuleOptions =>
+  ({
+    type: 'sqlite',
+    database: config.getDatabasePath(),
+    entities: [__dirname + '/../../../**/*.entity{.ts}'],
+    synchronize: true,
+    autoLoadEntities: true,
+    migrationsRun: true,
+    migrations: [__dirname + '/../../../infra/migrations/*{.ts}'],
+  }) as TypeOrmModuleOptions;
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [EnvironmentConfigModule],
+      inject: [EnvironmentConfigService],
+      useFactory: getTypeOrmModuleOptions,
+    }),
+  ],
+})
+export class TypeOrmConfigModule {}
