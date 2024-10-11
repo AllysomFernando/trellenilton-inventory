37de6a9551deb740b03b6cec0b9cdd4d84a96034
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-11 17:01:24 -0300

    feat: adicionar método findById à classe UserRepositoryOrm para busca de usuário por ID

--- src/infrastructures/repositories/user.repository.ts
+++ src/infrastructures/repositories/user.repository.ts
@@ -17,6 +17,11 @@ export class UserRepositoryOrm implements UserRepository {
     return users.map((user) => this.toUser(user));
   }
 
+  async findById(id: number): Promise<UserModel> {
+    const user = await this.userRepository.findOneBy({ id });
+    return this.toUser(user);
+  }
+
   private toUser(userEntity: User): UserModel {
     const user: UserModel = new UserModel();
 
