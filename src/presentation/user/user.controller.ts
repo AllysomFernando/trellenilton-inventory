2d1ca2dc9f55163d983a77a0b5ec11f265b07aa3
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-13 16:11:54 -0300

    feat: adicionar métodos para obter todos os usuários e criar um novo usuário no UserController

--- src/presentation/user/user.controller.ts
+++ src/presentation/user/user.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common'
+import { Controller, Get, Inject, Param, Post } from '@nestjs/common'
 import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase'
+import { GetAllUserUseCase } from '@/applications/usecases/user/getalluser.usecase'
+
 import { UseCaseProxy } from 'src/infrastructures/usecaseproxy/usecase-proxy'
 import { UsecaseProxyModule } from 'src/infrastructures/usecaseproxy/usecase-proxy.module'
 
 @Controller('user')
 export class UserController {
   constructor(
+    @Inject(UsecaseProxyModule.GET_ALL_USERS_USE_CASE)
+    private readonly getAllUsersUsecaseProxy: UseCaseProxy<GetAllUserUseCase.UseCase>,
     @Inject(UsecaseProxyModule.GET_USER_BY_ID_USE_CASE)
     private readonly getUserByIdUsecaseProxy: UseCaseProxy<GetUserByIdUseCase.UseCase>
   ) {}
-
+  @Get('/')
+  async getAllUsers() {
+    const result = await this.getAllUsersUsecaseProxy.getInstance().execute()
+    return {
+      status: 'OK',
+      code: 200,
+      message: 'Users found',
+      data: result
+    }
+  }
   @Get('/:id')
   async getUserById(@Param('id') id: number) {
     const result = await this.getUserByIdUsecaseProxy
@@ -20,6 +33,15 @@ export class UserController {
       code: 200,
       message: 'User found',
       data: result
+    }
+  }
+  @Post('/')
+  async createUser() {
+    return {
+      status: 'OK',
+      code: 200,
+      message: 'User created',
+      data: {}
     }
   }
 }
