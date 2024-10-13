aaf31e2edcdff35908e871cf523da9947c205194
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-13 15:26:22 -0300

    feat: adicionar caso de uso GetAllUser ao módulo UsecaseProxy

--- src/infrastructures/usecaseproxy/usecase-proxy.module.ts
+++ src/infrastructures/usecaseproxy/usecase-proxy.module.ts
@@ -2,6 +2,7 @@ import { DynamicModule, Module } from '@nestjs/common'
 import { EnvironmentConfigModule } from '../config/environment-config/environment-config.module'
 import { RepositoriesModule } from '../repositories/repository.modules'
 import { UserRepositoryOrm } from '../repositories/user.repository'
+import { GetAllUserUseCase } from '@/applications/usecases/user/getalluser.usecase'
 import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase'
 import { UseCaseProxy } from './usecase-proxy'
 
@@ -16,6 +17,12 @@ export class UsecaseProxyModule {
     return {
       module: UsecaseProxyModule,
       providers: [
+        {
+          inject: [UserRepositoryOrm],
+          provide: UsecaseProxyModule.GET_ALL_USERS_USE_CASE,
+          useFactory: (userRepository: UserRepositoryOrm) =>
+            new UseCaseProxy(new GetAllUserUseCase.UseCase(userRepository))
+        },
         {
           inject: [UserRepositoryOrm],
           provide: UsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
