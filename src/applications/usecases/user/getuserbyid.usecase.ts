c4bbe9d6f52337a1b5d64edf7556f47078acd7c0
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-13 15:22:23 -0300

    feat: implementar caso de uso GetUserById para recuperação de usuário por ID

---
+++ src/applications/usecases/user/getuserbyid.usecase.ts
@@ -0,0 +1,20 @@
+import { UserModel } from 'src/domain/models/user'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { UserRepository } from 'src/domain/repository/user.repository'
+
+export namespace GetUserByIdUseCase {
+  export type Input = {
+    id: number
+  }
+
+  export type Output = UserModel
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private userRepository: UserRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const entity = await this.userRepository.findById(input.id)
+      return entity
+    }
+  }
+}
