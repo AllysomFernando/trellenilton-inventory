6b1acf206c798f0b756014fde47684b2b62ced86
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-13 15:22:33 -0300

    feat: adicionar caso de uso GetUserById ao módulo UsecaseProxy

--- src/infrastructures/usecaseproxy/usecase-proxy.module.ts
+++ src/infrastructures/usecaseproxy/usecase-proxy.module.ts
@@ -1,15 +1,16 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { EnvironmentConfigModule } from '../config/environment-config/environment-config.module';
-import { RepositoriesModule } from '../repositories/repository.modules';
-import { UserRepositoryOrm } from '../repositories/user.repository';
-import { GetUserUseCase } from 'src/applications/usecases/user/getuser.usecase';
-import { UseCaseProxy } from './usecase-proxy';
+import { DynamicModule, Module } from '@nestjs/common'
+import { EnvironmentConfigModule } from '../config/environment-config/environment-config.module'
+import { RepositoriesModule } from '../repositories/repository.modules'
+import { UserRepositoryOrm } from '../repositories/user.repository'
+import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase'
+import { UseCaseProxy } from './usecase-proxy'
 
 @Module({
-  imports: [EnvironmentConfigModule, RepositoriesModule],
+  imports: [EnvironmentConfigModule, RepositoriesModule]
 })
 export class UsecaseProxyModule {
-  static GET_ALL_USERS_USE_CASE = 'getAllUsersUsecaseProxy';
+  static GET_ALL_USERS_USE_CASE = 'getAllUsersUsecaseProxy'
+  static GET_USER_BY_ID_USE_CASE = 'getUserByIdUsecaseProxy'
 
   static register(): DynamicModule {
     return {
@@ -17,12 +18,12 @@ export class UsecaseProxyModule {
       providers: [
         {
           inject: [UserRepositoryOrm],
-          provide: UsecaseProxyModule.GET_ALL_USERS_USE_CASE,
+          provide: UsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
-            new UseCaseProxy(new GetUserUseCase(userRepository)),
-        },
+            new UseCaseProxy(new GetUserByIdUseCase.UseCase(userRepository))
+        }
       ],
-      exports: [UsecaseProxyModule.GET_ALL_USERS_USE_CASE],
-    };
+      exports: [UsecaseProxyModule.GET_USER_BY_ID_USE_CASE]
+    }
   }
 }
