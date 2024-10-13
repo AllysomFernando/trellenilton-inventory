a6120a403f2225cb6076024ec30795d4ca08ae37
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-13 15:26:19 -0300

    refactor: renomear método getAllUser para findAll no UserRepositoryOrm

--- src/infrastructures/repositories/user.repository.ts
+++ src/infrastructures/repositories/user.repository.ts
@@ -1,35 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { UserRepository } from 'src/domain/repository/user.repository';
-import { User } from '../entities/user.entity';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserModel } from 'src/domain/models/user';
+import { Injectable } from '@nestjs/common'
+import { UserRepository } from 'src/domain/repository/user.repository'
+import { User } from '../entities/user.entity'
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserModel } from 'src/domain/models/user'
 
 @Injectable()
 export class UserRepositoryOrm implements UserRepository {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    private readonly userRepository: Repository<User>
   ) {}
 
-  async getAllUser(): Promise<User[]> {
-    const users = await this.userRepository.find();
-    return users.map((user) => this.toUser(user));
+  async findAll(): Promise<User[]> {
+    const users = await this.userRepository.find()
+    return users.map((user) => this.toUser(user))
   }
 
   async findById(id: number): Promise<UserModel> {
-    const user = await this.userRepository.findOneBy({ id });
-    return this.toUser(user);
+    const user = await this.userRepository.findOneBy({ id })
+    return this.toUser(user)
   }
 
   private toUser(userEntity: User): UserModel {
-    const user: UserModel = new UserModel();
+    const user: UserModel = new UserModel()
 
-    user.id = userEntity.id;
-    user.email = userEntity.email;
-    user.name = userEntity.name;
-    user.password = userEntity.password;
+    user.id = userEntity.id
+    user.email = userEntity.email
+    user.name = userEntity.name
+    user.password = userEntity.password
 
-    return user;
+    return user
   }
 }
