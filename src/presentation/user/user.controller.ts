6fc8163b3bc141a5fbdb2d5c5650b01639a4e51c
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-13 21:56:46 -0300

    feat: adicionar caso de uso CreateUser ao UserController

--- src/presentation/user/user.controller.ts
+++ src/presentation/user/user.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Inject, Param, Post } from '@nestjs/common'
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common'
 import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase'
 import { GetAllUserUseCase } from '@/applications/usecases/user/getalluser.usecase'
-
+import { CreateUserUseCase } from '@/applications/usecases/user/createuser.usecase'
 import { UseCaseProxy } from 'src/infrastructures/usecaseproxy/usecase-proxy'
 import { UsecaseProxyModule } from 'src/infrastructures/usecaseproxy/usecase-proxy.module'
 
@@ -11,7 +11,9 @@ export class UserController {
     @Inject(UsecaseProxyModule.GET_ALL_USERS_USE_CASE)
     private readonly getAllUsersUsecaseProxy: UseCaseProxy<GetAllUserUseCase.UseCase>,
     @Inject(UsecaseProxyModule.GET_USER_BY_ID_USE_CASE)
-    private readonly getUserByIdUsecaseProxy: UseCaseProxy<GetUserByIdUseCase.UseCase>
+    private readonly getUserByIdUsecaseProxy: UseCaseProxy<GetUserByIdUseCase.UseCase>,
+    @Inject(UsecaseProxyModule.CREATE_USER_USE_CASE)
+    private readonly createUserUsecaseProxy: UseCaseProxy<CreateUserUseCase.UseCase>
   ) {}
   @Get('/')
   async getAllUsers() {
@@ -36,12 +38,15 @@ export class UserController {
     }
   }
   @Post('/')
-  async createUser() {
+  async createUser(@Body() createUserDto: CreateUserUseCase.Input) {
+    const result = await this.createUserUsecaseProxy
+      .getInstance()
+      .execute(createUserDto)
     return {
       status: 'OK',
       code: 200,
       message: 'User created',
-      data: {}
+      data: result
     }
   }
 }
