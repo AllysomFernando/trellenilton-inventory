d9b89d1aced624b9c90a055ab76db82ddfcf2040
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-15 15:53:06 -0300

    feat: adicionar suporte ao caso de uso de exclusão de usuário no módulo de proxy

--- src/infrastructures/usecaseproxy/usecase-proxy.module.ts
+++ src/infrastructures/usecaseproxy/usecase-proxy.module.ts
@@ -7,6 +7,7 @@ import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.use
 import { CreateUserUseCase } from '@/applications/usecases/user/createuser.usecase'
 import { UseCaseProxy } from './usecase-proxy'
 import { UpdateUserUseCase } from '@/applications/usecases/user/updateuser.usecase'
+import { DeleteUserUseCase } from '@/applications/usecases/user/deleteuser.usecase'
 
 @Module({
   imports: [EnvironmentConfigModule, RepositoriesModule]
@@ -16,6 +17,7 @@ export class UsecaseProxyModule {
   static GET_USER_BY_ID_USE_CASE = 'getUserByIdUsecaseProxy'
   static CREATE_USER_USE_CASE = 'createUserUsecaseProxy'
   static UPDATE_USER_USE_CASE = 'updateUserUseCaseProxy'
+  static DELETE_USER_USE_CASE = 'deleteUserUseCaseProxy'
   static register(): DynamicModule {
     return {
       module: UsecaseProxyModule,
@@ -43,13 +45,20 @@ export class UsecaseProxyModule {
           provide: UsecaseProxyModule.UPDATE_USER_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
             new UseCaseProxy(new UpdateUserUseCase.UseCase(userRepository))
+        },
+        {
+          inject: [UserRepositoryOrm],
+          provide: UsecaseProxyModule.DELETE_USER_USE_CASE,
+          useFactory: (userRepository: UserRepositoryOrm) =>
+            new UseCaseProxy(new DeleteUserUseCase.UseCase(userRepository))
         }
       ],
       exports: [
         UsecaseProxyModule.GET_ALL_USERS_USE_CASE,
         UsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
         UsecaseProxyModule.CREATE_USER_USE_CASE,
-        UsecaseProxyModule.UPDATE_USER_USE_CASE
+        UsecaseProxyModule.UPDATE_USER_USE_CASE,
+        UsecaseProxyModule.DELETE_USER_USE_CASE
       ]
     }
   }
