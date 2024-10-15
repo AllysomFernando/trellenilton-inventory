c6d968ab2eb2517aadc0fb4e28b427741e20f525
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-15 15:16:33 -0300

    feat: adicionar caso de uso para exclusão de usuário

---
+++ src/applications/usecases/user/deleteuser.usecase.ts
@@ -0,0 +1,30 @@
+import { UserModel } from '@/domain/models/user'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { UserRepository } from '@/domain/repository/user.repository'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+
+export namespace DeleteUserUseCase {
+  export type Input = {
+    id: number
+  }
+
+  export type Output = void
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private userRepository: UserRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      if (!input.id) {
+        throw new Error('Id é obrigatório.')
+      }
+      try {
+        const entity = await this.userRepository.delete(input.id)
+        if (!entity) {
+          throw new BadRequestError('Falha ao deletar o usuário.')
+        }
+      } catch (e) {
+        throw new BadRequestError('Falha ao deletar o usuário.')
+      }
+    }
+  }
+}
