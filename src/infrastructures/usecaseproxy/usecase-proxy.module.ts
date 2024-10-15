4c56ed76c7c78e362359cf9359d6aeb25586d343
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-15 15:08:28 -0300

    feat: adicionar suporte para atualização de usuário no módulo UsecaseProxy

--- src/infrastructures/usecaseproxy/usecase-proxy.module.ts
+++ src/infrastructures/usecaseproxy/usecase-proxy.module.ts
@@ -6,6 +6,7 @@ import { GetAllUserUseCase } from '@/applications/usecases/user/getalluser.useca
 import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase'
 import { CreateUserUseCase } from '@/applications/usecases/user/createuser.usecase'
 import { UseCaseProxy } from './usecase-proxy'
+import { UpdateUserUseCase } from '@/applications/usecases/user/updateuser.usecase'
 
 @Module({
   imports: [EnvironmentConfigModule, RepositoriesModule]
@@ -14,7 +15,7 @@ export class UsecaseProxyModule {
   static GET_ALL_USERS_USE_CASE = 'getAllUsersUsecaseProxy'
   static GET_USER_BY_ID_USE_CASE = 'getUserByIdUsecaseProxy'
   static CREATE_USER_USE_CASE = 'createUserUsecaseProxy'
-
+  static UPDATE_USER_USE_CASE = 'updateUserUseCaseProxy'
   static register(): DynamicModule {
     return {
       module: UsecaseProxyModule,
@@ -36,12 +37,19 @@ export class UsecaseProxyModule {
           provide: UsecaseProxyModule.CREATE_USER_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
             new UseCaseProxy(new CreateUserUseCase.UseCase(userRepository))
+        },
+        {
+          inject: [UserRepositoryOrm],
+          provide: UsecaseProxyModule.UPDATE_USER_USE_CASE,
+          useFactory: (userRepository: UserRepositoryOrm) =>
+            new UseCaseProxy(new UpdateUserUseCase.UseCase(userRepository))
         }
       ],
       exports: [
         UsecaseProxyModule.GET_ALL_USERS_USE_CASE,
         UsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
-        UsecaseProxyModule.CREATE_USER_USE_CASE
+        UsecaseProxyModule.CREATE_USER_USE_CASE,
+        UsecaseProxyModule.UPDATE_USER_USE_CASE
       ]
     }
   }
