a9eb09b26b95ec1430498686d363652b4771ac70
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-15 14:55:40 -0300

    feat: adicionar método de atualização no repositório de usuário

--- src/domain/repository/user.repository.ts
+++ src/domain/repository/user.repository.ts
@@ -4,4 +4,5 @@ export interface UserRepository {
   findAll(): Promise<UserModel[]>
   findById(id: number): Promise<UserModel>
   save(user: UserModel): Promise<UserModel>
+  update(user: UserModel): Promise<UserModel>
 }
