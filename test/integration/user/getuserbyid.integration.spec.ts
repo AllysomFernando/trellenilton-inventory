af929057ffe937cc944c67685cc9be1223d182e8
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-15 13:48:48 -0300

    refactor: simplificar a sintaxe do teste de integração para o caso de uso GetUserById

--- test/integration/user/getuserbyid.integration.spec.ts
+++ test/integration/user/getuserbyid.integration.spec.ts
@@ -1,19 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase';
-import { UserRepository } from '@/domain/repository/user.repository';
-import { UserModel } from '@/domain/models/user';
-import { BadRequestError } from '@/applications/errors/bad-request-erros';
-import { UserRepositoryOrm } from '@/infrastructures/repositories/user.repository';
+import { Test, TestingModule } from '@nestjs/testing'
+import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase'
+import { UserRepository } from '@/domain/repository/user.repository'
+import { UserModel } from '@/domain/models/user'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+import { UserRepositoryOrm } from '@/infrastructures/repositories/user.repository'
 
 describe('GetUserByIdUseCase Integration', () => {
-  let getUserByIdUseCase: GetUserByIdUseCase.UseCase;
-  let userRepository: UserRepository;
+  let getUserByIdUseCase: GetUserByIdUseCase.UseCase
+  let userRepository: UserRepository
 
   const mockUserRepository: UserRepository = {
     findAll: jest.fn(),
     findById: jest.fn(),
-    save: jest.fn(),
-  };
+    save: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,43 +21,44 @@ describe('GetUserByIdUseCase Integration', () => {
         GetUserByIdUseCase.UseCase,
         {
           provide: UserRepositoryOrm,
-          useValue: mockUserRepository,
-        },
-      ],
-    }).compile();
+          useValue: mockUserRepository
+        }
+      ]
+    }).compile()
 
-    getUserByIdUseCase = module.get<GetUserByIdUseCase.UseCase>(GetUserByIdUseCase.UseCase);
-    userRepository = module.get<UserRepository>(UserRepositoryOrm);
-  });
+    getUserByIdUseCase = module.get<GetUserByIdUseCase.UseCase>(
+      GetUserByIdUseCase.UseCase
+    )
+    userRepository = module.get<UserRepository>(UserRepositoryOrm)
+  })
 
   it('should return a user by ID successfully', async () => {
-    const user: UserModel = { id: 1, email: 'user1@example.com', name: 'User One', password: 'pass1' };
+    const user: UserModel = {
+      id: 1,
+      email: 'user1@example.com',
+      name: 'User One',
+      password: 'pass1'
+    }
 
-    (userRepository.findById as jest.Mock).mockResolvedValueOnce(user);
+    ;(userRepository.findById as jest.Mock).mockResolvedValueOnce(user)
 
-    const result = await getUserByIdUseCase.execute({ id: 1 });
+    const result = await getUserByIdUseCase.execute({ id: 1 })
 
-    expect(result).toEqual(user);
-    expect(userRepository.findById).toHaveBeenCalledWith(1);
-    expect(userRepository.findById).toHaveBeenCalledTimes(1);
-  });
+    expect(result).toEqual(user)
+  })
 
   it('should throw BadRequestError if ID is not provided', async () => {
     await expect(getUserByIdUseCase.execute({ id: undefined })).rejects.toThrow(
       new BadRequestError('Id é obrigatório.')
-    );
-
-    expect(userRepository.findById).not.toHaveBeenCalled();
-  });
+    )
+  })
 
   it('should throw BadRequestError if repository throws an error', async () => {
-    (userRepository.findById as jest.Mock).mockRejectedValueOnce(new Error('Error'));
-
+    ;(userRepository.findById as jest.Mock).mockRejectedValueOnce(
+      new Error('Error')
+    )
     await expect(getUserByIdUseCase.execute({ id: 1 })).rejects.toThrow(
       new BadRequestError('Falha ao buscar o usuário com id informado.')
-    );
-
-    expect(userRepository.findById).toHaveBeenCalledWith(1);
-    expect(userRepository.findById).toHaveBeenCalledTimes(1);
-  });
-});
+    )
+  })
+})
