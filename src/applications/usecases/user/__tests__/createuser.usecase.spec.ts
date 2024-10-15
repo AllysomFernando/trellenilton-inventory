e5162ef04b515be3c113cc6ff899e0ef7c2fa604
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-15 15:10:31 -0300

    feat: adicionar método de atualização no repositório de usuário nos testes

--- src/applications/usecases/user/__tests__/createuser.usecase.spec.ts
+++ src/applications/usecases/user/__tests__/createuser.usecase.spec.ts
@@ -6,7 +6,8 @@ import { BadRequestError } from '@/applications/errors/bad-request-erros'
 const mockUserRepository: UserRepository = {
   findAll: jest.fn(),
   findById: jest.fn(),
-  save: jest.fn()
+  save: jest.fn(),
+  update: jest.fn()
 }
 
 describe('CreateUserUseCase', () => {
@@ -33,7 +34,6 @@ describe('CreateUserUseCase', () => {
     user.email = input.email
     user.name = input.name
     user.password = input.password
-
     ;(mockUserRepository.save as jest.Mock).mockResolvedValueOnce(user)
 
     const result = await createUserUseCase.execute(input)
