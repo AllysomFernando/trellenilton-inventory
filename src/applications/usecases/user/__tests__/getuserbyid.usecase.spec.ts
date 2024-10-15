adcd1491aaf624f96d148fa63ef02d3c9a1cdc1c
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-15 12:46:21 -0300

    test: melhorar testes do GetUserByIdUseCase para verificar erros e chamadas ao repositório

--- src/applications/usecases/user/__tests__/getuserbyid.usecase.spec.ts
+++ src/applications/usecases/user/__tests__/getuserbyid.usecase.spec.ts
@@ -24,28 +24,28 @@ describe('GetUserByIdUseCase', () => {
       password: 'password123'
     }
 
-    ;(mockUserRepository.findById as jest.Mock).mockResolvedValueOnce(user) 
+    ;(mockUserRepository.findById as jest.Mock).mockResolvedValueOnce(user)
 
     const result = await getUserByIdUseCase.execute({ id: 1 })
 
     expect(result).toEqual(user)
-    expect(mockUserRepository.findById).toHaveBeenCalledWith(1) 
+    expect(mockUserRepository.findById).toHaveBeenCalledWith(1)
     expect(mockUserRepository.findById).toHaveBeenCalledTimes(1)
   })
 
   it('should throw BadRequestError if id is not provided', async () => {
-    ;(mockUserRepository.findById as jest.Mock).mockResolvedValueOnce(null)
 
     await expect(getUserByIdUseCase.execute({ id: undefined })).rejects.toThrow(
       new BadRequestError('Id é obrigatório.')
     )
 
+    // Verifica se o repositório não foi chamado
     expect(mockUserRepository.findById).not.toHaveBeenCalled()
   })
 
   it('should throw BadRequestError if an error occurs while fetching the user', async () => {
     ;(mockUserRepository.findById as jest.Mock).mockRejectedValueOnce(
-      new Error('Error')
+      new Error('Repository Error')
     )
 
     await expect(getUserByIdUseCase.execute({ id: 1 })).rejects.toThrow(
