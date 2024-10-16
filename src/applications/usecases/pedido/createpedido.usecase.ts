3598e689c85cafc713edbfcd34c096756a776bf9
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 16:44:10 -0300

    feat: implementar caso de uso para criação de pedidos com validação de entrada

---
+++ src/applications/usecases/pedido/createpedido.usecase.ts
@@ -0,0 +1,32 @@
+import { PedidoModels } from '@/domain/models/pedido'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { PedidoRepository } from '@/domain/repository/pedido.repository'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+
+export namespace CreatePedidoUseCase {
+  export type Input = {
+    data: Date
+    clienteId: number
+    status: string
+    total: number
+  }
+
+  export type Output = PedidoModels
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private pedidoRepository: PedidoRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      if (!input.data || !input.clienteId || !input.status || !input.total) {
+        throw new BadRequestError(
+          'Data, clienteId, status e total são obrigatórios.'
+        )
+      }
+      const pedido = new PedidoModels()
+      pedido.data = input.data
+      pedido.clienteId = input.clienteId
+      pedido.status = input.status
+      pedido.total = input.total
+    }
+  }
+}
