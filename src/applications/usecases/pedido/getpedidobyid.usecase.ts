3eeaa8b2888c5f43aace7b1287921d31c764f6bd
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 19:55:50 -0300

    feat: implementar caso de uso para obter pedido por ID com tratamento de erro

---
+++ src/applications/usecases/pedido/getpedidobyid.usecase.ts
@@ -0,0 +1,31 @@
+import { PedidoModels } from '@/domain/models/pedido'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { PedidoRepository } from '@/domain/repository/pedido.repository'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+
+export namespace GetPedidoByIdUseCase {
+  export type Input = {
+    id: number
+  }
+
+  export type Output = PedidoModels
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private pedidoRepository: PedidoRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      if (!input.id) {
+        throw new BadRequestError('Id do pedido é obrigatório.')
+      }
+      try {
+        const entity = await this.pedidoRepository.findById(input.id)
+        if (!entity) {
+          throw new BadRequestError('Pedido não encontrado.')
+        }
+        return entity
+      } catch (error) {
+        throw new BadRequestError('Falha ao buscar o pedido.')
+      }
+    }
+  }
+}
