ab9e8c016d88335181b997c4e87a7f5a3b2a1792
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 15:57:30 -0300

    feat: adicionar interface ClienteRepository com métodos CRUD

---
+++ src/domain/repository/cliente.repository.ts
@@ -0,0 +1,9 @@
+import { ClienteModel } from '../models/cliente'
+
+export interface ClienteRepository {
+  findAll(): Promise<ClienteModel[]>
+  findById(id: number): Promise<ClienteModel>
+  save(cliente: ClienteModel): Promise<ClienteModel>
+  update(cliente: ClienteModel): Promise<ClienteModel>
+  delete(id: number): Promise<boolean>
+}
