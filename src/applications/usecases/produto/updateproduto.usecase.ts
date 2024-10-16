c7aa96f638cee7e27f331eedcf7c35d39d3b7734
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 10:45:51 -0300

    feat: implementar caso de uso para atualização de produtos com validação de entrada

---
+++ src/applications/usecases/produto/updateproduto.usecase.ts
@@ -0,0 +1,56 @@
+import { ProdutoModel } from '@/domain/models/produto'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+import { ProdutoRepository } from '@/domain/repository/produto.repository'
+
+export namespace UpdateProdutoUseCase {
+  export type Input = {
+    id: number
+    name: string
+    description: string
+    price: number
+    quantity: number
+    image: string
+    fornecedorId: number
+  }
+
+  export type Output = ProdutoModel
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private produtoRepository: ProdutoRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      if (!input.id) {
+        throw new Error('Id é obrigatório.')
+      }
+      //TODO: implementar logica de verificar se o produto existe antes de continuar, se nao existir retornar um erro
+      if (
+        !input.name &&
+        !input.description &&
+        !input.price &&
+        !input.quantity &&
+        !input.image &&
+        !input.fornecedorId
+      ) {
+        throw new BadRequestError('Informe ao menos um campo para atualização.')
+      }
+      const produto = new ProdutoModel()
+      produto.id = input.id
+      produto.name = input.name
+      produto.description = input.description
+      produto.price = input.price
+      produto.quantity = input.quantity
+      produto.image = input.image
+      produto.fornecedorId = input.fornecedorId
+      try {
+        const entity = await this.produtoRepository.update(produto)
+        if (!entity) {
+          throw new BadRequestError('Falha ao atualizar o produto.')
+        }
+        return entity
+      } catch (e) {
+        throw new BadRequestError('Falha ao atualizar o produto.')
+      }
+    }
+  }
+}
