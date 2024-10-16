337aec26f2be51be4a80a981956e9627003c8183
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 16:22:33 -0300

    feat: implementar caso de uso para atualização de cliente com validação de entrada

--- src/applications/usecases/cliente/updateclient.usecase.ts
+++ src/applications/usecases/cliente/updateclient.usecase.ts
@@ -1,9 +1,40 @@
+import { ClienteModel } from '@/domain/models/cliente'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { ClienteRepository } from '@/domain/repository/cliente.repository'
+
 export namespace UpdateClienteUseCase {
   export type Input = {
-    id: string
+    id: number
     nome: string
-    cpf_cpnj: string
+    cpf_cnpj: string
     contato: string
-    telefone: string
+    endereco: string
+  }
+
+  export type Output = ClienteModel
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private clienteRepository: ClienteRepository) {}
+
+    async execute(input: Input): Promise<ClienteModel> {
+      if (!input.id) {
+        throw new Error('Id é obrigatório.')
+      }
+      const cliente = new ClienteModel()
+      cliente.id = input.id
+      cliente.name = input.nome
+      cliente.cpf_cnpj = input.cpf_cnpj
+      cliente.contato = input.contato
+      cliente.endereco = input.endereco
+      try {
+        const entity = await this.clienteRepository.update(cliente)
+        if (!entity) {
+          throw new Error('Falha ao atualizar o cliente.')
+        }
+        return entity
+      } catch (e) {
+        throw new Error('Falha ao atualizar o cliente.')
+      }
+    }
   }
 }
