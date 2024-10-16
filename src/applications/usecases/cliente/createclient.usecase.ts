a08b3f0ca367c277a0033a4818904cd5bc0c764f
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 16:17:26 -0300

    feat: implementar caso de uso para criação de cliente

---
+++ src/applications/usecases/cliente/createclient.usecase.ts
@@ -0,0 +1,38 @@
+import { ClienteModel } from '@/domain/models/cliente'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { ClienteRepository } from '@/domain/repository/cliente.repository'
+
+export namespace CreateClientUseCase {
+  export type Input = {
+    name: string
+    cpf_cnpj: string
+    endereco: string
+    contato: string
+  }
+
+  export type Output = ClienteModel
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private clienteRepository: ClienteRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      if (!input.name || !input.cpf_cnpj || !input.endereco || !input.contato) {
+        throw new Error('Nome, CPF/CNPJ, Endereço e Contato são obrigatórios.')
+      }
+      const cliente = new ClienteModel()
+      cliente.name = input.name
+      cliente.cpf_cnpj = input.cpf_cnpj
+      cliente.endereco = input.endereco
+      cliente.contato = input.contato
+      try {
+        const entity = await this.clienteRepository.save(cliente)
+        if (!entity) {
+          throw new Error('Falha ao salvar o cliente.')
+        }
+        return entity
+      } catch (e) {
+        throw new Error('Falha ao salvar o cliente.')
+      }
+    }
+  }
+}
