8d92c8717168902257e220e44f981fead2679979
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 15:49:46 -0300

    feat: adicionar repositório de produtos ao caso de uso de exclusão de usuário

--- src/applications/usecases/user/deleteuser.usecase.ts
+++ src/applications/usecases/user/deleteuser.usecase.ts
@@ -1,6 +1,7 @@
 import { UseCase as DefaultUseCase } from '../use-case'
 import { UserRepository } from '@/domain/repository/user.repository'
 import { BadRequestError } from '@/applications/errors/bad-request-erros'
+import { ProdutoRepository } from '@/domain/repository/produto.repository'
 
 export namespace DeleteUserUseCase {
   export type Input = {
@@ -10,12 +11,16 @@ export namespace DeleteUserUseCase {
   export type Output = void
 
   export class UseCase implements DefaultUseCase<Input, Output> {
-    constructor(private userRepository: UserRepository) {}
+    constructor(
+      private userRepository: UserRepository,
+      private produtoRepository: ProdutoRepository
+    ) {}
 
     async execute(input: Input): Promise<Output> {
       if (!input.id) {
         throw new Error('Id é obrigatório.')
       }
+      const produtos = await this.produtoRepository.findById(input.id)
       try {
         const entity = await this.userRepository.delete(input.id)
         if (!entity) {
