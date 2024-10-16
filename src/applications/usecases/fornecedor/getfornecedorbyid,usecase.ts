cadddae09eed937c5141135ebe9df0b0de2af608
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 15:40:32 -0300

    feat: implementar caso de uso para obter fornecedor por ID

---
+++ src/applications/usecases/fornecedor/getfornecedorbyid,usecase.ts
@@ -0,0 +1,34 @@
+import { FornecedorModel } from '@/domain/models/fornecedor'
+import { UseCase as DefaultUseCase } from '../use-case'
+import { FornecedorRepository } from '@/domain/repository/fornecedor.repository'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+
+export namespace GetFornecedorByIdUseCase {
+  export type Input = {
+    id: number
+  }
+
+  export type Output = FornecedorModel
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private fornecedorRepository: FornecedorRepository) {}
+    async execute(input: Input): Promise<Output> {
+      if (!input.id) {
+        throw new BadRequestError('ID eh obrigatório.')
+      }
+      try {
+        const entity = await this.fornecedorRepository.findById(input.id)
+        if (!entity) {
+          throw new BadRequestError(
+            'Falha ao buscar o fornecedor com id informado.'
+          )
+        }
+        return entity
+      } catch (e) {
+        throw new BadRequestError(
+          'Falha ao buscar o fornecedor com id informado.'
+        )
+      }
+    }
+  }
+}
