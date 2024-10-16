d4e8ca02f0ec081ac5d1dfb0c81634a41b684a58
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 15:22:26 -0300

    feat: implementar validação completa para CNPJ

--- src/applications/validators/cnpj.validators.ts
+++ src/applications/validators/cnpj.validators.ts
@@ -1,6 +1,37 @@
 export class CNPJValidator {
   static isValid(cnpj: string): boolean {
-    const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
-    return cnpjRegex.test(cnpj)
+    if (cnpj.length !== 14) {
+      return false
+    }
+    if (cnpj === '00000000000000') {
+      return false
+    }
+    let soma = 0
+    let resto
+    for (let i = 1; i <= 12; i++) {
+      soma = soma + parseInt(cnpj.substring(i - 1, i)) * (14 - i)
+    }
+    resto = soma % 11
+    if (resto < 2) {
+      resto = 0
+    } else {
+      resto = 11 - resto
+    }
+    if (resto !== parseInt(cnpj.substring(12, 13))) {
+      return false
+    }
+    soma = 0
+    for (let i = 1; i <= 13; i++) {
+      soma = soma + parseInt(cnpj.substring(i - 1, i)) * (15 - i)
+    }
+    resto = soma % 11
+    if (resto < 2) {
+      resto = 0
+    } else {
+      resto = 11 - resto
+    }
+    if (resto !== parseInt(cnpj.substring(13, 14))) {
+      return false
+    }
   }
 }
