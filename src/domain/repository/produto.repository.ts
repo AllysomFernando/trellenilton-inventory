fbcb19e9e864dae0b9863016990f6f61dd04a1a5
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 10:27:20 -0300

    feat: criar interface ProdutoRepository com métodos para manipulação de produtos

---
+++ src/domain/repository/produto.repository.ts
@@ -0,0 +1,9 @@
+import { ProdutoModel } from '../models/produto'
+
+export interface ProdutoRepository {
+  findAll(): Promise<ProdutoModel[]>
+  findById(id: number): Promise<ProdutoModel>
+  save(produto: ProdutoModel): Promise<ProdutoModel>
+  update(produto: ProdutoModel): Promise<ProdutoModel>
+  delete(id: number): Promise<boolean>
+}
