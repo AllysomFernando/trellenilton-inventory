5192d5775e0fac0248e9c1463f440aeebf6bc6c9
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-17 09:39:33 -0300

    refactor: remove unnecessary semicolons in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UsecaseProxyModule } from './infrastructures/usecaseproxy/usecase-proxy.module';
-import { UserModule } from './presentation/user/user.module';
-import { EnvironmentConfigModule } from './infrastructures/config/environment-config/environment-config.module';
+import { Module } from '@nestjs/common'
+import { UsecaseProxyModule } from './infrastructures/usecaseproxy/usecase-proxy.module'
+import { UserModule } from './presentation/user/user.module'
+import { EnvironmentConfigModule } from './infrastructures/config/environment-config/environment-config.module'
 
 @Module({
-  imports: [UsecaseProxyModule.register(), UserModule, EnvironmentConfigModule],
+  imports: [UsecaseProxyModule.register(), UserModule, EnvironmentConfigModule]
 })
 export class AppModule {}
