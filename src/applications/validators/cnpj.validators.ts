dbad6bd0bb0f7ea3884df11bc21c6f22b3099f8a
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-17 10:00:06 -0300

    feat: improve CNPJ validation logic in CNPJValidator

--- src/applications/validators/cnpj.validators.ts
+++ src/applications/validators/cnpj.validators.ts
@@ -1,37 +1,47 @@
 export class CNPJValidator {
   static isValid(cnpj: string): boolean {
-    if (cnpj.length !== 14) {
+    cnpj = cnpj.replace(/[^\d]+/g, '')
+
+    if (cnpj.length !== 14 || /^(\d)\1+$/.test(cnpj)) {
       return false
     }
-    if (cnpj === '00000000000000') {
-      return false
-    }
+
     let soma = 0
     let resto
-    for (let i = 1; i <= 12; i++) {
-      soma = soma + parseInt(cnpj.substring(i - 1, i)) * (14 - i)
+
+    const pesosPrimeiroDV = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    for (let i = 0; i < 12; i++) {
+      soma += parseInt(cnpj.charAt(i)) * pesosPrimeiroDV[i]
     }
+
     resto = soma % 11
     if (resto < 2) {
       resto = 0
     } else {
       resto = 11 - resto
     }
-    if (resto !== parseInt(cnpj.substring(12, 13))) {
+
+    if (resto !== parseInt(cnpj.charAt(12))) {
       return false
     }
+
     soma = 0
-    for (let i = 1; i <= 13; i++) {
-      soma = soma + parseInt(cnpj.substring(i - 1, i)) * (15 - i)
+    const pesosSegundoDV = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    for (let i = 0; i < 13; i++) {
+      soma += parseInt(cnpj.charAt(i)) * pesosSegundoDV[i]
     }
+
     resto = soma % 11
     if (resto < 2) {
       resto = 0
     } else {
       resto = 11 - resto
     }
-    if (resto !== parseInt(cnpj.substring(13, 14))) {
+
+    if (resto !== parseInt(cnpj.charAt(13))) {
       return false
     }
+
+    return true
   }
 }
