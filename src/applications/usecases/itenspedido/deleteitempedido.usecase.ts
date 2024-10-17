131eef39efec3f41094a19e927dca0b1892abac0
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-17 17:16:09 -0300

    feat: implement DeleteItemPedidoUseCase with error handling and validation

--- src/applications/usecases/itenspedido/deleteitempedido.usecase.ts
+++ src/applications/usecases/itenspedido/deleteitempedido.usecase.ts
@@ -1,7 +1,30 @@
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+import { UseCase as DefaultUseCase } from '@/applications/usecases/use-case'
+import { ItemPedidoRepository } from '@/domain/repository/itempedido.repository'
+
 export namespace DeleteItemPedidoUseCase {
   export type Input = {
-    id: string
+    id: number
   }
 
   export type Output = void
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private itemPedidoRepository: ItemPedidoRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const itemPedido = await this.itemPedidoRepository.findById(input.id)
+      if (!itemPedido) {
+        throw new BadRequestError('Item do pedido não encontrado.')
+      }
+      try {
+        const deleted = await this.itemPedidoRepository.delete(input.id)
+        if (!deleted) {
+          throw new BadRequestError('Erro ao deletar item do pedido.')
+        }
+      } catch (e) {
+        throw new BadRequestError('Erro ao deletar item do pedido.')
+      }
+    }
+  }
 }
