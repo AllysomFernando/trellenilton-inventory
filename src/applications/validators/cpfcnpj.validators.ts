b75e8ca1d0e61220a35f3de8a8d35402df3a1cad
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-17 10:58:49 -0300

    feat: add CPF and CNPJ validation utility class

---
+++ src/applications/validators/cpfcnpj.validators.ts
@@ -0,0 +1,23 @@
+import { CNPJValidator } from '@/applications/validators/cnpj.validators'
+import { CPFValidator } from './cpf.validators'
+
+export class IsCPFOrCnpj {
+  static isCpf(cpfCnpj: string): boolean {
+    return cpfCnpj.length === 11
+  }
+
+  static isCnpj(cpfCnpj: string): boolean {
+    return cpfCnpj.length === 14
+  }
+
+  static isValid(cpfCnpj: string): boolean {
+    cpfCnpj = cpfCnpj.replace(/[^\d]+/g, '')
+
+    if (this.isCpf(cpfCnpj)) {
+      return CPFValidator.isValid(cpfCnpj)
+    } else if (this.isCnpj(cpfCnpj)) {
+      return CNPJValidator.isValid(cpfCnpj)
+    }
+    return false
+  }
+}
