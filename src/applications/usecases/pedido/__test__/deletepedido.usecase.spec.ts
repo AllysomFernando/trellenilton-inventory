8dc8b63ac62ed007e76c50ce7c08c5040f33a968
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-16 21:42:13 -0300

    feat: add unit tests for DeletePedidoUseCase with error handling

---
+++ src/applications/usecases/pedido/__test__/deletepedido.usecase.spec.ts
@@ -0,0 +1,46 @@
+import { PedidoRepository } from '@/domain/repository/pedido.repository'
+import { DeletePedidoUseCase } from '../deletepedido.usecase'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
+
+describe('DeletePedidoUseCase', () => {
+  let pedidoRepository: PedidoRepository
+  let deletePedidoUseCase: DeletePedidoUseCase.UseCase
+
+  beforeEach(() => {
+    pedidoRepository = {
+      delete: jest.fn()
+    } as unknown as PedidoRepository
+
+    deletePedidoUseCase = new DeletePedidoUseCase.UseCase(pedidoRepository)
+  })
+
+  it('should throw an error if id is not provided', async () => {
+    await expect(deletePedidoUseCase.execute({ id: null })).rejects.toThrow(
+      'ID eh obrigatório.'
+    )
+  })
+
+  it('should throw BadRequestError if pedido deletion fails', async () => {
+    ;(pedidoRepository.delete as jest.Mock).mockResolvedValue(null)
+    await expect(deletePedidoUseCase.execute({ id: 1 })).rejects.toThrow(
+      BadRequestError
+    )
+  })
+
+  it('should delete pedido successfully', async () => {
+    ;(pedidoRepository.delete as jest.Mock).mockResolvedValue(true)
+    await expect(
+      deletePedidoUseCase.execute({ id: 1 })
+    ).resolves.toBeUndefined()
+    expect(pedidoRepository.delete).toHaveBeenCalledWith(1)
+  })
+
+  it('should throw BadRequestError if an exception occurs during deletion', async () => {
+    ;(pedidoRepository.delete as jest.Mock).mockRejectedValue(
+      new Error('Some error')
+    )
+    await expect(deletePedidoUseCase.execute({ id: 1 })).rejects.toThrow(
+      BadRequestError
+    )
+  })
+})
