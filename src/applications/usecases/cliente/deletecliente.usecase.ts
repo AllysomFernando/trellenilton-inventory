1b9c0a5778204f7636dad86feb58d3d62b645ad6
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-17 17:29:00 -0300

    feat: implement DeleteClienteUseCase with validation for existing pedidos

--- src/applications/usecases/cliente/deletecliente.usecase.ts
+++ src/applications/usecases/cliente/deletecliente.usecase.ts
@@ -1,6 +1,7 @@
 import { ClienteRepository } from '@/domain/repository/cliente.repository'
 import { UseCase as DefaultUseCase } from '../use-case'
 import { BadRequestError } from '@/applications/errors/bad-request-erros'
+import { PedidoRepository } from '@/domain/repository/pedido.repository'
 
 export namespace DeleteClienteUseCase {
   export type Input = {
@@ -10,13 +11,19 @@ export namespace DeleteClienteUseCase {
   export type Output = void
 
   export class UseCase implements DefaultUseCase<Input, Output> {
-    constructor(private clienteRepository: ClienteRepository) {}
+    constructor(
+      private clienteRepository: ClienteRepository,
+      private pedidoRepository: PedidoRepository
+    ) {}
 
     async execute(input: Input): Promise<Output> {
       if (!input.id) {
         throw new Error('Id é obrigatório.')
       }
-      //TODO: quando implementar o usecase de itenspedidos verificar se o cliente possui algum item relacionado com ele.
+      const verifyPedido = await this.pedidoRepository.findByClienteId(input.id)
+      if (verifyPedido.length > 0) {
+        throw new BadRequestError('Cliente possui pedidos.')
+      }
       try {
         const entity = await this.clienteRepository.findById(input.id)
         if (!entity) {
