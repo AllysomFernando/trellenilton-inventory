d7eda5ecd587f30e2ecf13b634613049132bd92b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-17 11:15:54 -0300

    feat: enhance CreateClienteUseCase with CPF/CNPJ validation and error handling

--- src/applications/usecases/cliente/createcliente.usecase.ts
+++ src/applications/usecases/cliente/createcliente.usecase.ts
@@ -2,6 +2,7 @@ import { ClienteModel } from '@/domain/models/cliente'
 import { UseCase as DefaultUseCase } from '../use-case'
 import { ClienteRepository } from '@/domain/repository/cliente.repository'
 import { IsCPFOrCnpj } from '@/applications/validators/cpfcnpj.validators'
+import { BadRequestError } from '@/applications/errors/bad-request-erros'
 
 export namespace CreateClienteUseCase {
   export type Input = {
@@ -20,7 +21,10 @@ export namespace CreateClienteUseCase {
       if (!input.name || !input.cpf_cnpj || !input.endereco || !input.contato) {
         throw new Error('Nome, CPF/CNPJ, Endereço e Contato são obrigatórios.')
       }
-      IsCPFOrCnpj.isValid(input.cpf_cnpj)
+      const isValidCpfCnpj = IsCPFOrCnpj.isValid(input.cpf_cnpj)
+      if (!isValidCpfCnpj) {
+        throw new BadRequestError('CPF ou CNPJ inválido.')
+      }
       const cliente = new ClienteModel()
       cliente.name = input.name
       cliente.cpf_cnpj = input.cpf_cnpj
@@ -29,11 +33,11 @@ export namespace CreateClienteUseCase {
       try {
         const entity = await this.clienteRepository.save(cliente)
         if (!entity) {
-          throw new Error('Falha ao salvar o cliente.')
+          throw new BadRequestError('Falha ao salvar o cliente.')
         }
         return entity
       } catch (e) {
-        throw new Error('Falha ao salvar o cliente.')
+        throw new BadRequestError('Falha ao salvar o cliente.')
       }
     }
   }
