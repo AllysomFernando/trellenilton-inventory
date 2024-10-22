ac4cbd759acb00c741a25abdd1c9b881d45c5d84
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 14:29:18 -0300

    refactor: rename parameters in PedidoRepository methods for clarity

--- src/domain/repository/pedido.repository.ts
+++ src/domain/repository/pedido.repository.ts
@@ -3,8 +3,8 @@ import { PedidoModel } from '../models/pedido'
 export interface PedidoRepository {
   findAll(): Promise<PedidoModel[]>
   findById(id: number): Promise<PedidoModel>
-  save(produto: PedidoModel): Promise<PedidoModel>
-  update(produto: PedidoModel): Promise<PedidoModel>
+  save(pedido: PedidoModel): Promise<PedidoModel>
+  update(pedido: PedidoModel): Promise<PedidoModel>
   delete(id: number): Promise<boolean>
   findByClienteId(clienteId: number): Promise<PedidoModel[]>
 }
