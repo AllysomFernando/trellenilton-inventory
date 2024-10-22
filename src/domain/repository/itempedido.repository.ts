649fb83a2d01944759f302bdf3e2d826bfa62b77
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 14:11:26 -0300

    refactor: update ItemPedidoRepository to use ItemPedidoModel for save and update methods

--- src/domain/repository/itempedido.repository.ts
+++ src/domain/repository/itempedido.repository.ts
@@ -1,19 +1,8 @@
 import { ItemPedidoModel } from '../models/itemPedido'
 
 export interface ItemPedidoRepository {
-  save: (
-    pedidoId: number,
-    produtoId: number,
-    quantidade: number,
-    precoUnitario: number
-  ) => Promise<ItemPedidoModel>
-  update: (
-    id: number,
-    pedidoId: number,
-    produtoId: number,
-    quantidade: number,
-    precoUnitario: number
-  ) => Promise<ItemPedidoModel>
+  save: (itemPedido: ItemPedidoModel) => Promise<ItemPedidoModel>
+  update: (itemPedido: ItemPedidoModel) => Promise<ItemPedidoModel>
   findById: (id: number) => Promise<ItemPedidoModel>
   delete: (id: number) => Promise<boolean>
 }
