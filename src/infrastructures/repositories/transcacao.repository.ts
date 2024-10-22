9a4998a76f5365c3a8d53811362ec9412c188218
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 14:51:00 -0300

    feat: implement TransacaoRepositoryOrm with CRUD operations for Transacao

---
+++ src/infrastructures/repositories/transcacao.repository.ts
@@ -0,0 +1,58 @@
+import { TransacaoRepository } from '@/domain/repository/transacao.repository'
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Transacao } from '../entities/transacao.entity'
+import { Repository } from 'typeorm'
+import { TransacaoEnum, TransacaoModel } from '@/domain/models/transacao'
+
+@Injectable()
+export class TransacaoRepositoryOrm implements TransacaoRepository {
+  constructor(
+    @InjectRepository(Transacao)
+    private readonly transacaoRepository: Repository<Transacao>
+  ) {}
+
+  async findAll(): Promise<Transacao[]> {
+    const transacoes = await this.transacaoRepository.find()
+    if (!transacoes || transacoes.length === 0) {
+      return []
+    }
+    return transacoes
+  }
+
+  async findById(id: number): Promise<TransacaoModel> {
+    const transacao = await this.transacaoRepository.findOneBy({ id })
+    if (!transacao) {
+      return null
+    }
+    return this.toTransacao(transacao)
+  }
+
+  async save(transacao: TransacaoModel): Promise<TransacaoModel> {
+    const entity = this.transacaoRepository.create(transacao)
+    await this.transacaoRepository.save(entity)
+    return this.toTransacao(entity)
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const entity = await this.transacaoRepository.findOneBy({ id })
+    if (!entity) {
+      return false
+    }
+    await this.transacaoRepository.remove(entity)
+    return true
+  }
+
+  private toTransacao(transacaoEntity: Transacao): TransacaoModel {
+    const transacao = new TransacaoModel()
+
+    transacao.id = transacaoEntity.id
+    transacao.data = transacaoEntity.data
+    transacao.produtoId = transacaoEntity.produtoId
+    transacao.pedidoId = transacaoEntity.pedidoId
+    transacao.tipo = transacaoEntity.tipo as TransacaoEnum
+    transacao.valor = transacaoEntity.valor
+
+    return transacao
+  }
+}
