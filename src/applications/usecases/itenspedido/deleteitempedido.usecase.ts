81d7a977e58f060fa98af5e948672dd3e557214e
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 11:49:04 -0300

    feat: update DeleteItemPedidoUseCase to return deletion status and validate input

--- src/applications/usecases/itenspedido/deleteitempedido.usecase.ts
+++ src/applications/usecases/itenspedido/deleteitempedido.usecase.ts
@@ -7,12 +7,15 @@ export namespace DeleteItemPedidoUseCase {
     id: number
   }
 
-  export type Output = void
+  export type Output = boolean
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private itemPedidoRepository: ItemPedidoRepository) {}
 
     async execute(input: Input): Promise<Output> {
+      if (!input.id) {
+        throw new BadRequestError('Id é obrigatório.')
+      }
       const itemPedido = await this.itemPedidoRepository.findById(input.id)
       if (!itemPedido) {
         throw new BadRequestError('Item do pedido não encontrado.')
@@ -22,6 +25,7 @@ export namespace DeleteItemPedidoUseCase {
         if (!deleted) {
           throw new BadRequestError('Erro ao deletar item do pedido.')
         }
+        return deleted
       } catch (e) {
         throw new BadRequestError('Erro ao deletar item do pedido.')
       }
