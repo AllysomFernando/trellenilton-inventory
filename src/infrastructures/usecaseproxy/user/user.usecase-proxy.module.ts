e72489070c5db1ddc4b198459b75e18f2fdc4a74
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 14:58:11 -0300

    refactor: rename UsecaseProxyModule to UserUsecaseProxyModule and update import paths

--- src/infrastructures/usecaseproxy/user/user.usecase-proxy.module.ts
+++ src/infrastructures/usecaseproxy/user/user.usecase-proxy.module.ts
@@ -1,18 +1,18 @@
 import { DynamicModule, Module } from '@nestjs/common'
-import { EnvironmentConfigModule } from '../config/environment-config/environment-config.module'
-import { RepositoriesModule } from '../repositories/repository.modules'
-import { UserRepositoryOrm } from '../repositories/user.repository'
+import { EnvironmentConfigModule } from '../../config/environment-config/environment-config.module'
+import { RepositoriesModule } from '../../repositories/repository.modules'
+import { UserRepositoryOrm } from '../../repositories/user.repository'
 import { GetAllUserUseCase } from '@/applications/usecases/user/getalluser.usecase'
 import { GetUserByIdUseCase } from '@/applications/usecases/user/getuserbyid.usecase'
 import { CreateUserUseCase } from '@/applications/usecases/user/createuser.usecase'
-import { UseCaseProxy } from './usecase-proxy'
+import { UseCaseProxy } from '../usecase-proxy'
 import { UpdateUserUseCase } from '@/applications/usecases/user/updateuser.usecase'
 import { DeleteUserUseCase } from '@/applications/usecases/user/deleteuser.usecase'
 
 @Module({
   imports: [EnvironmentConfigModule, RepositoriesModule]
 })
-export class UsecaseProxyModule {
+export class UserUsecaseProxyModule {
   static GET_ALL_USERS_USE_CASE = 'getAllUsersUsecaseProxy'
   static GET_USER_BY_ID_USE_CASE = 'getUserByIdUsecaseProxy'
   static CREATE_USER_USE_CASE = 'createUserUsecaseProxy'
@@ -20,29 +20,29 @@ export class UsecaseProxyModule {
   static DELETE_USER_USE_CASE = 'deleteUserUseCaseProxy'
   static register(): DynamicModule {
     return {
-      module: UsecaseProxyModule,
+      module: UserUsecaseProxyModule,
       providers: [
         {
           inject: [UserRepositoryOrm],
-          provide: UsecaseProxyModule.GET_ALL_USERS_USE_CASE,
+          provide: UserUsecaseProxyModule.GET_ALL_USERS_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
             new UseCaseProxy(new GetAllUserUseCase.UseCase(userRepository))
         },
         {
           inject: [UserRepositoryOrm],
-          provide: UsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
+          provide: UserUsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
             new UseCaseProxy(new GetUserByIdUseCase.UseCase(userRepository))
         },
         {
           inject: [UserRepositoryOrm],
-          provide: UsecaseProxyModule.CREATE_USER_USE_CASE,
+          provide: UserUsecaseProxyModule.CREATE_USER_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
             new UseCaseProxy(new CreateUserUseCase.UseCase(userRepository))
         },
         {
           inject: [UserRepositoryOrm],
-          provide: UsecaseProxyModule.UPDATE_USER_USE_CASE,
+          provide: UserUsecaseProxyModule.UPDATE_USER_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
             new UseCaseProxy(
               new UpdateUserUseCase.UseCase(
@@ -53,17 +53,17 @@ export class UsecaseProxyModule {
         },
         {
           inject: [UserRepositoryOrm],
-          provide: UsecaseProxyModule.DELETE_USER_USE_CASE,
+          provide: UserUsecaseProxyModule.DELETE_USER_USE_CASE,
           useFactory: (userRepository: UserRepositoryOrm) =>
             new UseCaseProxy(new DeleteUserUseCase.UseCase(userRepository))
         }
       ],
       exports: [
-        UsecaseProxyModule.GET_ALL_USERS_USE_CASE,
-        UsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
-        UsecaseProxyModule.CREATE_USER_USE_CASE,
-        UsecaseProxyModule.UPDATE_USER_USE_CASE,
-        UsecaseProxyModule.DELETE_USER_USE_CASE
+        UserUsecaseProxyModule.GET_ALL_USERS_USE_CASE,
+        UserUsecaseProxyModule.GET_USER_BY_ID_USE_CASE,
+        UserUsecaseProxyModule.CREATE_USER_USE_CASE,
+        UserUsecaseProxyModule.UPDATE_USER_USE_CASE,
+        UserUsecaseProxyModule.DELETE_USER_USE_CASE
       ]
     }
   }
