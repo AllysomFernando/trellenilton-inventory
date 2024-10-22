4ee666c3ee910c4573a5a8c321548c1cca413a60
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 14:55:31 -0300

    test: update DeleteItemPedidoUseCase tests for improved error handling and success scenarios

--- src/applications/usecases/itenspedido/__test__/deleteitempedido.usecase.spec.ts
+++ src/applications/usecases/itenspedido/__test__/deleteitempedido.usecase.spec.ts
@@ -16,9 +16,7 @@ describe('DeleteItemPedidoUseCase', () => {
       itemPedidoRepository
     )
     findItemPedidoByIdUseCase = {
-      execute: jest
-        .fn()
-        .mockRejectedValue(new Error('Item do pedido não encontrado.'))
+      execute: jest.fn().mockResolvedValue({ id: 1 })
     } as unknown as GetItemPedidoByIdUseCase.UseCase
   })
   it('should throw an error if id is not provided', async () => {
@@ -27,17 +25,24 @@ describe('DeleteItemPedidoUseCase', () => {
     )
   })
   it('should throw BadRequestError if itemPedido was not found', async () => {
+    ;(findItemPedidoByIdUseCase.execute as jest.Mock).mockRejectedValue(
+      new Error('Item do pedido não encontrado.')
+    )
     await expect(findItemPedidoByIdUseCase.execute({ id: 1 })).rejects.toThrow(
       'Item do pedido não encontrado.'
     )
   })
   it('should throw BadRequestError if itemPedido deletion fails', async () => {
+    ;(itemPedidoRepository.findById as jest.Mock).mockResolvedValue({ id: 1 })
     ;(itemPedidoRepository.delete as jest.Mock).mockResolvedValue(false)
     await expect(deleteItemPedidoUseCase.execute({ id: 1 })).rejects.toThrow(
       'Erro ao deletar item do pedido.'
     )
   })
   it('should delete itemPedido successfully', async () => {
+    ;(findItemPedidoByIdUseCase.execute as jest.Mock).mockResolvedValue({
+      id: 1
+    })
     ;(itemPedidoRepository.delete as jest.Mock).mockResolvedValue(true)
     await expect(
       deleteItemPedidoUseCase.execute({ id: 1 })
