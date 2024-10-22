b0912fdfbc48bbe0d07c6c6d5de5409dff6c2e60
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 20:18:04 -0300

    feat: register ItensPedidoUsecaseProxyModule and add ItemPedidoController to ItemPedidoModule

--- src/presentation/itemPedido/itempedido.module.ts
+++ src/presentation/itemPedido/itempedido.module.ts
@@ -1,7 +1,9 @@
+import { ItensPedidoUsecaseProxyModule } from '@/infrastructures/usecaseproxy/itenspedido/itenspedido.usecase-proxy.modules'
 import { Module } from '@nestjs/common'
+import { ItemPedidoController } from './itempedido.controller'
 
 @Module({
-  imports: [],
-  controllers: []
+  imports: [ItensPedidoUsecaseProxyModule.register()],
+  controllers: [ItemPedidoController]
 })
 export class ItemPedidoModule {}
