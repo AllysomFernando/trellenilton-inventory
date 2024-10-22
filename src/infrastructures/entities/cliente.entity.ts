87087c36c6e01f8d2b163fe206f1e9f3887b14ef
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 13:53:39 -0300

    feat: add Cliente entity with properties for id, name, cpf_cnpj, endereco, and contato

---
+++ src/infrastructures/entities/cliente.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('cliente')
+export class Cliente {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @Column('varchar')
+  name: string
+
+  @Column('varchar', [{ unique: true }])
+  cpf_cnpj: string
+
+  @Column('varchar')
+  endereco: string
+
+  @Column('varchar')
+  contato: string
+}
