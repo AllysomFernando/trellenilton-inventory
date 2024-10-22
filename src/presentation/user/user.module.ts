7b335c148f81ab4a15168f6a4a5a2d603ded8500
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 17:31:17 -0300

    fix: update import path for UserUsecaseProxyModule in user.module.ts

--- src/presentation/user/user.module.ts
+++ src/presentation/user/user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { UserUsecaseProxyModule } from 'src/infrastructures/usecaseproxy/user/user.usecase-proxy.module'
+import { UserUsecaseProxyModule } from '@/infrastructures/usecaseproxy/user/user.usecase-proxy.module'
 import { UserController } from './user.controller'
 
 @Module({
