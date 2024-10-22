ad4cbbef99d186791d7182b906e00e1f5064cfd9
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-22 17:47:18 -0300

    feat: register TransacaoController in TransacaoModule

--- src/presentation/transacao/transacao.module.ts
+++ src/presentation/transacao/transacao.module.ts
@@ -1,8 +1,9 @@
 import { TransacoesUsecaseProxyModule } from '@/infrastructures/usecaseproxy/transacoes/transacoes.usecase-proxy.modules'
 import { Module } from '@nestjs/common'
+import { TransacaoController } from './transacao.controller'
 
 @Module({
   imports: [TransacoesUsecaseProxyModule.register()],
-  controllers: []
+  controllers: [TransacaoController]
 })
 export class TransacaoModule {}
