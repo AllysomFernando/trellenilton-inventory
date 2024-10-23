0d6cdc1f86c391640728d8a9e5f42452362b983e
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-23 10:29:48 -0300

    feat: create migration file for Fornecedor entity with table structure

--- database/migrations/1729688613791-Fornecedor.ts
+++ database/migrations/1729688613791-Fornecedor.ts
@@ -1,11 +1,46 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
-export class Fornecedor1729688613791 implements MigrationInterface {
+export class FornecedorMigration1629688613791 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'fornecedor',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'cnpj',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true
+          },
+          {
+            name: 'endereco',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'contato',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true
+          }
+        ]
+      })
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('fornecedor')
+  }
 }
