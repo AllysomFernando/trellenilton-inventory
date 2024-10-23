6b6cb38c029fe7528c50e44c70a5953000646289
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-23 10:13:36 -0300

    feat: update Pedido entity to establish relationships with Cliente and ItemPedido

--- src/infrastructures/entities/pedido.entity.ts
+++ src/infrastructures/entities/pedido.entity.ts
@@ -1,13 +1,23 @@
 import { PedidoStatus } from '@/domain/models/pedido'
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+import { Cliente } from './cliente.entity'
+import { ItemPedido } from './itemPedido.entity'
 
 @Entity('pedido')
 export class Pedido {
   @PrimaryGeneratedColumn('increment')
   id: number
 
-  @Column('int')
-  clienteId: number
+  @ManyToOne(() => Cliente, (cliente) => cliente.pedidos)
+  @JoinColumn({ name: 'clienteId' })
+  cliente: Cliente
 
   @Column('datetime')
   data: Date
@@ -17,4 +27,7 @@ export class Pedido {
 
   @Column('decimal')
   total: number
+
+  @OneToMany(() => ItemPedido, (itemPedido) => itemPedido.pedidoId)
+  itens: ItemPedido[]
 }
