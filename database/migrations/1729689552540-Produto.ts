7cd71315ebbb3ae927689b9930b7261747705d2e
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-23 10:35:37 -0300

    feat: create Produto migration with table structure and foreign key to Fornecedor

--- database/migrations/1729689552540-Produto.ts
+++ database/migrations/1729689552540-Produto.ts
@@ -1,11 +1,77 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey
+} from 'typeorm'
 
 export class Produto1729689552540 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'produto',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            isNullable: false
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+            isNullable: false
+          },
+          {
+            name: 'image',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'fornecedorId',
+            type: 'int',
+            isNullable: false
+          }
+        ]
+      })
+    )
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'produto',
+      new TableForeignKey({
+        columnNames: ['fornecedorId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'fornecedor',
+        onDelete: 'CASCADE'
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('produto')
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('fornecedorId') !== -1
+    )
+    if (foreignKey) {
+      await queryRunner.dropForeignKey('produto', foreignKey)
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+    await queryRunner.dropTable('produto')
+  }
 }
