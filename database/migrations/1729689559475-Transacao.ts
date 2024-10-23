96e81e11d3ab271f3ff33f40d50f8d3014016a73
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-23 10:19:29 -0300

    feat: add migration file for Transacao entity

---
+++ database/migrations/1729689559475-Transacao.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Transacao1729689559475 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
