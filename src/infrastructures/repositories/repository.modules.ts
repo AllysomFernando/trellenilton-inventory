a27fd9597cef132574cc329499a9a36f8d9a4e91
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-23 09:53:01 -0300

    feat: expand RepositoriesModule to include additional entity repositories

--- src/infrastructures/repositories/repository.modules.ts
+++ src/infrastructures/repositories/repository.modules.ts
@@ -1,12 +1,51 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmConfigModule } from '../config/typeorm/typeorm.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
-import { UserRepositoryOrm } from './user.repository';
+import { Module } from '@nestjs/common'
+import { TypeOrmConfigModule } from '../config/typeorm/typeorm.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../entities/user.entity'
+import { UserRepositoryOrm } from './user.repository'
+import { Cliente } from '../entities/cliente.entity'
+import { ItemPedido } from '../entities/itemPedido.entity'
+import { Fornecedor } from '../entities/fornecedor.entity'
+import { Pedido } from '../entities/pedido.entity'
+import { Produto } from '../entities/produto.entity'
+import { Transacao } from '../entities/transacao.entity'
+import { TransacaoRepositoryOrm } from './transcacao.repository'
+import { ClienteRepositoryOrm } from './cliente.repository'
+import { PedidoRepositoryOrm } from './pedido.repository'
+import { ProdutoRepositoryOrm } from './produto.repository'
+import { ItemPedidoRepositoryOrm } from './itempedido.repository'
+import { FornecedorRepositoryOrm } from './fornecedor.repository'
 
 @Module({
-  imports: [TypeOrmConfigModule, TypeOrmModule.forFeature([User])],
-  providers: [UserRepositoryOrm],
-  exports: [UserRepositoryOrm],
+  imports: [
+    TypeOrmConfigModule,
+    TypeOrmModule.forFeature([
+      User,
+      Cliente,
+      ItemPedido,
+      Fornecedor,
+      Pedido,
+      Produto,
+      Transacao
+    ])
+  ],
+  providers: [
+    UserRepositoryOrm,
+    TransacaoRepositoryOrm,
+    ClienteRepositoryOrm,
+    PedidoRepositoryOrm,
+    ProdutoRepositoryOrm,
+    ItemPedidoRepositoryOrm,
+    FornecedorRepositoryOrm
+  ],
+  exports: [
+    UserRepositoryOrm,
+    TransacaoRepositoryOrm,
+    ClienteRepositoryOrm,
+    PedidoRepositoryOrm,
+    ProdutoRepositoryOrm,
+    ItemPedidoRepositoryOrm,
+    FornecedorRepositoryOrm
+  ]
 })
 export class RepositoriesModule {}
