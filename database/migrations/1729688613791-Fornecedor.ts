91aa320a015bd1b74d863670e6aaa2b97207040e
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-23 10:13:39 -0300

    feat: add migration for Fornecedor entity

---
+++ database/migrations/1729688613791-Fornecedor.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Fornecedor1729688613791 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
