bcd2e195467526c5a2a56ad5a20320c754916951
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-28 16:45:05 -0300

    refactor: import UsuarioEnum in UserModel for user type classification

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,12 +1,9 @@
-export enum UsuarioEnum {
-  Admin = 'Admin',
-  Usuario = 'Usuario'
-}
+import { UsuarioEnum } from '@/applications/enum/user.enum'
 
 export class UserModel {
-  id: number;
-  email: string;
-  name: string;
-  password: string;
-  tipo: UsuarioEnum;
+  id: number
+  email: string
+  name: string
+  password: string
+  tipo: UsuarioEnum
 }
