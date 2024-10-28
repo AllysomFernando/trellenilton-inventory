81e3261ea8cffdade8aacde22743330790f5f317
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-28 15:43:32 -0300

    feat: add tipo field to UserModel for user type classification

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,6 +1,12 @@
+export enum UsuarioEnum {
+  Admin = 'Admin',
+  Usuario = 'Usuario'
+}
+
 export class UserModel {
   id: number;
   email: string;
   name: string;
   password: string;
+  tipo: UsuarioEnum;
 }
